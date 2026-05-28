424d14ab0895de61b3c6ca65d318ead7bb03eae4
PorteDeChar <felixfontaine0@gmail.com>
2026-05-28 10:46:36 -0700

    debut incorporation regex dans conditions

--- validation.js
+++ validation.js
@@ -1,7 +1,23 @@
 let mdp = document.querySelector("#password").textContent//zone text mdp
+let courriel = document.querySelector("#email").textContent//zone de text de email
+let mdpconf = document.querySelector("#mdp_confirm").textContent//zone de text confiurmation mdp
 
 function VerificationMDP(mdp) {
-    if (mdp.length < 8 || !mdp.incldes(" ") || mdp.){
+    if (mdp.length < 8 || mdp.includes(" ") || !mdp.test(/[A-Za-z]/) || !mdp.test(/[0-9]/)) {
+        document.querySelector("#submit").addEventListener("click", () => {
 
+        })
+    }
+}
+
+function ValidationMail(mail) {
+    if (mail.test(/^ [^\s@]+ @ [^\s@]+ \. [^\s@]+ $/)) {//^ = début chaine \ = changele sens caratere apres "s" devient s qui veux dire espace "." devient un . litteral et pas nimporte quel caractere $ =- fin de la chaine
+
+    }
+}
+
+function ValidationMDPConfirmation(mdp, mdpconf) {
+    if (mdp = mdpconf) {
+        
     }
 }
