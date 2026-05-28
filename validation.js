27c255a04880974b2ac3c48c9c391977ad807fcd
PorteDeChar <felixfontaine0@gmail.com>
2026-05-28 12:56:36 -0700

    presque fini formulaire. pas sur de comprendre comment le send ou ne pas le send. might be dans les cours de cote serveur

--- validation.js
+++ validation.js
@@ -1,23 +1,25 @@
-let mdp = document.querySelector("#password").textContent//zone text mdp
-let courriel = document.querySelector("#email").textContent//zone de text de email
-let mdpconf = document.querySelector("#mdp_confirm").textContent//zone de text confiurmation mdp
+let mdp = document.querySelector("#password").value;//zone text mdp
+let courriel = document.querySelector("#email").value;//zone de text de email
+let mdpconf = document.querySelector("#mdp_confirm").value;//zone de text confiurmation mdp
+let btntest = document.querySelector("#submit");
+    btntest.addEventListener("click", VerificationMDP);
+let erreur = [];
 
 function VerificationMDP(mdp) {
-    if (mdp.length < 8 || mdp.includes(" ") || !mdp.test(/[A-Za-z]/) || !mdp.test(/[0-9]/)) {
-        document.querySelector("#submit").addEventListener("click", () => {
-
-        })
+    //if (mdp.length < 8 || mdp.includes(" ") || !mdp.test(/[A-Za-z]/) || !mdp.test(/[0-9]/))
+    if (/^ (?=.*[A-Za-z])(?=.*[0-9])\S{8,}$/.test(mdp)) {//regex -> ? = verifie si suite conforme a condition .* = lettre/chiffre peux se trouver nimporte ou
+        erreur.push("Le mot de passe est invalide");
+        }
     }
-}
 
 function ValidationMail(mail) {
-    if (mail.test(/^ [^\s@]+ @ [^\s@]+ \. [^\s@]+ $/)) {//^ = début chaine \ = changele sens caratere apres "s" devient s qui veux dire espace "." devient un . litteral et pas nimporte quel caractere $ =- fin de la chaine
-
+    if (/^[^\S@]+@[^\S@]+\.[^\S@]+$/.text(mail)) {//regex -> ^ = début chaine \ = changele sens caratere apres "s" devient s qui veux dire espace "." devient un . litteral et pas nimporte quel caractere $ =- fin de la chaine
+        erreur.push("L'adresse courriel n'est pas une adresse courriel valide");
     }
 }
 
 function ValidationMDPConfirmation(mdp, mdpconf) {
-    if (mdp = mdpconf) {
-        
+    if (mdp !== mdpconf) {
+        erreur.push("La confirmation du mot de passe n'est pas valide");
     }
 }
