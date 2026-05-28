c1b4ca629272e1aa4ad5da832afb93ce9699e659
PorteDeChar <felixfontaine0@gmail.com>
2026-05-28 13:19:30 -0700

    fix validation et changement du id de mot de passe dans inscription (html et css) pour uniformiser avec connexion.

--- validation.js
+++ validation.js
@@ -1,25 +1,51 @@
-let mdp = document.querySelector("#password").value;//zone text mdp
-let courriel = document.querySelector("#email").value;//zone de text de email
-let mdpconf = document.querySelector("#mdp_confirm").value;//zone de text confiurmation mdp
-let btntest = document.querySelector("#submit");
-    btntest.addEventListener("click", VerificationMDP);
+const champsmdp = document.querySelector("#password");//zone text mdp
+const champscourriel = document.querySelector("#email");//zone de text de email
+const champsmdpconf = document.querySelector("#mdp_confirm");//zone de text confiurmation mdp
+const champsbtntest = document.querySelector("#submit");
+
 let erreur = [];
 
-function VerificationMDP(mdp) {
-    //if (mdp.length < 8 || mdp.includes(" ") || !mdp.test(/[A-Za-z]/) || !mdp.test(/[0-9]/))
-    if (/^ (?=.*[A-Za-z])(?=.*[0-9])\S{8,}$/.test(mdp)) {//regex -> ? = verifie si suite conforme a condition .* = lettre/chiffre peux se trouver nimporte ou
-        erreur.push("Le mot de passe est invalide");
-        }
+function verification(){
+    const mdp = champsmdp.value;
+    if (mdp.length < 8){
+        erreur.push("Le mot de passe est trop petit");
+    }else if (mdp.includes(" ")){
+        erreur.push("Espace n'est pas permit dans le mot de passe");
+    }else if (!mdp.test(/[A-Za-z]/)){
+        erreur.push("Il doit y avoir au moins 1 lettre dans le mot de passe");
+    }else if (!mdp.test(/[0-9]/)){
+        erreur.push("Il doit y avoir au moins 1 chiffre dans le mot de passe");
     }
 
-function ValidationMail(mail) {
-    if (/^[^\S@]+@[^\S@]+\.[^\S@]+$/.text(mail)) {//regex -> ^ = début chaine \ = changele sens caratere apres "s" devient s qui veux dire espace "." devient un . litteral et pas nimporte quel caractere $ =- fin de la chaine
+    const mail = champscourriel.value;
+    if (!(/^[^\S@]+@[^\S@]+\.[^\S@]+$/.text(mail))) {
         erreur.push("L'adresse courriel n'est pas une adresse courriel valide");
     }
-}
 
-function ValidationMDPConfirmation(mdp, mdpconf) {
-    if (mdp !== mdpconf) {
+    const mdpconf = champsmdpconf.value;
+    if (mdp !== mdpconf){
         erreur.push("La confirmation du mot de passe n'est pas valide");
     }
 }
+
+//function VerificationMDP(mdp) {
+    //if (mdp.length < 8 || mdp.includes(" ") || !mdp.test(/[A-Za-z]/) || !mdp.test(/[0-9]/))
+  //  if (/^ (?=.*[A-Za-z])(?=.*[0-9])\S{8,}$/.test(mdp)) {//regex -> ? = verifie si suite conforme a condition .* = lettre/chiffre peux se trouver nimporte ou
+    //    erreur.push("Le mot de passe est invalide");
+      //  }
+    //}
+
+//function ValidationMail(mail) {
+  //  if (/^[^\S@]+@[^\S@]+\.[^\S@]+$/.text(mail)) {//regex -> ^ = début chaine \ = changele sens caratere apres "s" devient s qui veux dire espace "." devient un . litteral et pas nimporte quel caractere $ =- fin de la chaine
+    //    erreur.push("L'adresse courriel n'est pas une adresse courriel valide");
+    //}
+//}
+
+//function ValidationMDPConfirmation(mdp, mdpconf) {
+  //  if (mdp !== mdpconf) {
+    //    erreur.push("La confirmation du mot de passe n'est pas valide");
+    //}
+//}
+
+
+btntest.addEventListener("click", Verification);
