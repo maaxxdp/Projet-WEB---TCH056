539762cec2c5158038715eb1d32f15a3f874f94b
PorteDeChar <felixfontaine0@gmail.com>
2026-05-28 14:06:43 -0700

    debut du travail sur les filtres. ajout de data-etat sur les differents projets de facon a pouvoir les racorde par la suite selon ce dit etat.

---
+++ app.js
@@ -0,0 +1,10 @@
+const champrecherche = document.querySelector("#recherche");
+const checkbox = document.getElementsByName("etat");//prend les 3 boutons et utiliser un switch case pour la fonction
+const btnsubmit = document.querySelector("#submit");
+
+
+btnsubmit.addEventListener("click", Filtre());
+
+function Filtre() {
+    
+}
