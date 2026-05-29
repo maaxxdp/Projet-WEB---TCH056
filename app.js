4d4ece5c3a0c989f68bf3eab81f00738a1f050e9
PorteDeChar <felixfontaine0@gmail.com>
2026-05-29 05:53:51 -0700

    debut du chargement dynamique des projets. Creation d'un "template" qui va creer une carte de projet comme celles qu'on a manuellement creer

--- app.js
+++ app.js
@@ -7,4 +7,39 @@ btnsubmit.addEventListener("click", Filtre());
 
 function Filtre() {
     
-}
+}//il faut attendre que projet.js soit complete pour pouvoir utiliser le json
+
+
+//creation dynamique projet
+function nvxprojet(projet) {
+    const division = document.createElement("div");
+    division.className = "project-card";
+    division.setAttribute("date-ville", projet.ville);//(nomattribut, valeur)
+    division.setAttribute("data-etat", projet.etat);
+
+    const image = document.createElement("img");
+    image.className = "project-img";
+    image.src = projet.image
+    image.alt = projet.titre
+
+    const titre = document.createElement("h4");
+    const lien =document.createElement("a");
+    lien.href = "details.html?id=" + projet.id;
+    lien.textContent = projet.titre;
+    titre.appendChild(lien);//mets le a dans le h4
+
+    const description = document.createElement("p")
+    description.textContent = projet.description
+
+    const ville = document.createElement("p")
+    ville.textContent = "Ville: " + projet.ville
+
+    const etat = document.createElement("p")
+    etat.textContent = "État: " + projet.etat
+
+    projet.appendChild(image)
+    projet.appendChild(titre)
+    projet.appendChild(description)
+    projet.appendChild(ville)
+    projet.appendChild(etat)
+}   
